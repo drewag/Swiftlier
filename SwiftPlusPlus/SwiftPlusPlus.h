dad7e2e188eda5e53a88de451ea205b79da6ab2f
Andrew Wagner <andrew@drewag.me>
2014-06-07 12:34:04 -0600

    [FC] Initial Commit

---
+++ SwiftPlusPlus/SwiftPlusPlus.h
@@ -0,0 +1,19 @@
+//
+//  SwiftPlusPlus.h
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 6/7/14.
+//  Copyright (c) 2014 Drewag LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftPlusPlus.
+FOUNDATION_EXPORT double SwiftPlusPlusVersionNumber;
+
+//! Project version string for SwiftPlusPlus.
+FOUNDATION_EXPORT const unsigned char SwiftPlusPlusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftPlusPlus/PublicHeader.h>
+
+
