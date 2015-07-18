9b4fb42b563497658a4130bb13197084db7b40a4
Andrew J Wagner <andrew@drewagllc.com>
2015-07-17 21:12:21 -0600

    [FC] Added an email validation extension to String

--- SwiftPlusPlus/String+SwiftPlusPlus.swift
+++ SwiftPlusPlus/String+SwiftPlusPlus.swift
@@ -47,6 +47,13 @@ extension String {
         pos = advance(pos, index)
         return self.substringToIndex(pos)
     }
+
+    public var isValidEmail: Bool {
+        let emailRegEx = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$"
+
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluateWithObject(self)
+    }
 }
 
 public func /(lhs: String, rhs: String) -> String {
