27d08b77a10b17718f0e2a720aafaaf10330df5f
Andrew J Wagner <andrew@drewagllc.com>
2015-09-10 22:47:40 -0600

    [FC] Allow capturing an image from a view

---
+++ SwiftPlusPlus/iOS/UIView+ImageCapture.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+ImageCapture.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 9/10/15.
+//  Copyright © 2015 Drewag LLC. All rights reserved.
+//
+
+import Foundation
+
+extension UIView {
+    func captureImage() -> UIImage {
+        UIGraphicsBeginImageContextWithOptions(self.bounds.size, true, 1)
+        self.drawViewHierarchyInRect(self.bounds, afterScreenUpdates: true)
+        return UIGraphicsGetImageFromCurrentImageContext()
+    }
+}
