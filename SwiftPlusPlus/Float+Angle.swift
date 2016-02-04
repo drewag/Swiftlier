bc4c2ddc35c1112536b5bea9e1e2a93f16ac5440
Andrew J Wagner <andrew@drewagllc.com>
2016-02-03 21:14:54 -0700

    [FC] Added helper to Float to convert angles

---
+++ SwiftPlusPlus/Float+Angle.swift
@@ -0,0 +1,19 @@
+//
+//  Float+Angle.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 2/3/16.
+//  Copyright © 2016 Drewag LLC. All rights reserved.
+//
+
+import Foundation
+
+extension Float {
+    var toRadians: Float {
+        return self * Float(M_PI) / 180.0
+    }
+
+    var toDegrees: Float {
+        return self * 180.0 / Float(M_PI)
+    }
+}
