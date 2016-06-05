2bbc39fc0212caf54d6f0fa023c465f2054d3c8e
Andrew J Wagner <andrew@drewagllc.com>
2016-06-04 19:51:13 -0600

    [FC] Allow specifying source rect and arrow direction for popovers

--- SwiftPlusPlus/iOS/UIViewController+Helpers.swift
+++ SwiftPlusPlus/iOS/UIViewController+Helpers.swift
@@ -9,16 +9,19 @@
 import Foundation
 
 extension UIViewController {
-    public func presentPopoverViewController(viewController: UIViewController, fromSourceView sourceView: UIView) {
+    public func present(popoverViewController viewController: UIViewController, fromSourceView sourceView: UIView, sourceRect: CGRect? = nil, permittedArrowDirections: UIPopoverArrowDirection = .Any) {
         viewController.modalPresentationStyle = .Popover
 
         self.presentViewController(viewController, animated: true, completion: nil)
 
-        viewController.popoverPresentationController!.permittedArrowDirections = .Any
+        viewController.popoverPresentationController!.permittedArrowDirections = permittedArrowDirections
         viewController.popoverPresentationController!.sourceView = sourceView
+        if let sourceRect = sourceRect {
+            viewController.popoverPresentationController!.sourceRect = sourceRect
+        }
     }
 
-    public func presentOverlayViewController(viewController: UIViewController) {
+    public func present(overlayViewController viewController: UIViewController) {
         viewController.modalPresentationStyle = .OverCurrentContext
         viewController.modalTransitionStyle = .CrossDissolve
 
