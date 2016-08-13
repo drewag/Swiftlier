ab8a9aa03dea1bf94185827bb0f80adf8d8a843b
Andrew J Wagner <andrew@drewagllc.com>
2016-08-13 01:53:23 -0600

    [NF] Allow specifying an observable array to maintain an order

--- SwiftPlusPlus/ObservableArray.swift
+++ SwiftPlusPlus/ObservableArray.swift
@@ -10,10 +10,12 @@ public final class ObservableArray<Element> {
     public typealias DidInsert = (Element, at: Int) -> ()
     public typealias DidRemove = (Element, at: Int) -> ()
     public typealias DidRemoveAll = (oldValues: [Element]) -> ()
-    public typealias ObservationHandlers = (insert: DidInsert?, remove: DidRemove?, removeAll: DidRemoveAll?)
+    public typealias DidMove = (Element, from: Int, to: Int) -> ()
+    public typealias ObservationHandlers = (insert: DidInsert?, remove: DidRemove?, removeAll: DidRemoveAll?, didMove: DidMove?)
 
     private var observers: [(observer: WeakWrapper, handlers: [ObservationHandlers])] = []
     private var onHasObserversChanged: ((Bool) -> ())?
+    private var isOrderedBefore: ((lhs: Element, rhs: Element) -> Bool)?
 
     public private(set) var values: [Element]
 
@@ -21,8 +23,14 @@ public final class ObservableArray<Element> {
         self.init([])
     }
 
-    public init(_ values: [Element], onHasObserversChanged: ((Bool) -> ())? = nil) {
+    public init(
+        _ values: [Element],
+        enforceOrder isOrderedBefore: ((lhs: Element, rhs: Element) -> Bool)? = nil,
+        onHasObserversChanged: ((Bool) -> ())? = nil
+        )
+    {
         self.values = values
+        self.isOrderedBefore = isOrderedBefore
         self.onHasObserversChanged = onHasObserversChanged
     }
 
@@ -30,14 +38,20 @@ public final class ObservableArray<Element> {
         observer observer: AnyObject,
         onDidInsert: DidInsert? = nil,
         onDidRemove: DidRemove? = nil,
-        onDidRemoveAll: DidRemoveAll? = nil
+        onDidRemoveAll: DidRemoveAll? = nil,
+        onDidMove: DidMove? = nil
         )
     {
         guard onDidInsert != nil || onDidRemove != nil || onDidRemoveAll != nil else {
             return
         }
 
-        let handlers: ObservationHandlers = (insert: onDidInsert, remove: onDidRemove, removeAll: onDidRemoveAll)
+        let handlers: ObservationHandlers = (
+            insert: onDidInsert,
+            remove: onDidRemove,
+            removeAll: onDidRemoveAll,
+            didMove: onDidMove
+        )
         if let index = self.index(ofObserver: observer) {
             self.observers[index].handlers.append(handlers)
         }
@@ -60,6 +74,18 @@ public final class ObservableArray<Element> {
     }
 
     public func append(element: Element) {
+        if let isOrderedBefore = self.isOrderedBefore {
+            for (index, otherElement) in self.values.enumerate() {
+                if isOrderedBefore(lhs: element, rhs: otherElement) {
+                    self.values.insert(element, atIndex: index)
+                    self.executeWithAllHandlers({ handler in
+                        handler.insert?(element, at: index)
+                    })
+                    return
+                }
+            }
+        }
+
         self.values.append(element)
         let index = self.values.count - 1
         self.executeWithAllHandlers({ handler in
@@ -68,6 +94,10 @@ public final class ObservableArray<Element> {
     }
 
     public func insert(element: Element, at index: Int) {
+        if self.isOrderedBefore != nil {
+            self.append(element)
+        }
+
         self.values.insert(element, atIndex: index)
         self.executeWithAllHandlers({ handler in
             handler.insert?(element, at: index)
@@ -88,6 +118,41 @@ public final class ObservableArray<Element> {
         self.executeWithAllHandlers({ handler in
             handler.removeAll?(oldValues: oldValues)
         })
+    }
+
+    public func resort() {
+        guard let isOrderedBefore = self.isOrderedBefore else {
+            return
+        }
+
+        var unsortedTemplate = [(id: Int, element: Element)]()
+        for (index, element) in self.values.enumerate() {
+            let value = (id: index, element: element)
+            unsortedTemplate.append(value)
+        }
+        let sortedTemplate = unsortedTemplate.sort({ isOrderedBefore(lhs: $0.element, rhs: $1.element) })
+
+        var toIndex = self.values.count - 1
+        for (sortedId, _) in sortedTemplate.reverse() {
+            var fromIndex = 0
+            for (unsortedId, _) in unsortedTemplate {
+                if sortedId == unsortedId {
+                    if fromIndex != toIndex {
+                        // move
+                        let element = self.values.removeAtIndex(fromIndex)
+                        self.values.insert(element, atIndex: toIndex)
+                        let template = unsortedTemplate.removeAtIndex(fromIndex)
+                        unsortedTemplate.insert(template, atIndex: toIndex)
+                        self.executeWithAllHandlers({ handler in
+                            handler.didMove?(element, from: fromIndex, to: toIndex)
+                        })
+                    }
+                    break
+                }
+                fromIndex += 1
+            }
+            toIndex -= 1
+        }
     }
 }
 
