d85e1d34a8ba1e095ef23c111d279624976a0b77
Andrew Wagner <andrew@Andrews-MacBook-Pro.local>
2016-10-15 17:17:56 -0600

    [NF] Added an inWords property to bool

---
+++ SwiftPlusPlus/Bool+SwiftPlusPlus.swift
@@ -0,0 +1,13 @@
+//
+//  Bool+SwiftPlusPlus.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 10/14/16.
+//  Copyright © 2016 Drewag LLC. All rights reserved.
+//
+
+extension Bool {
+    public var inWords: String {
+        return self ? "Yes" : "No"
+    }
+}
