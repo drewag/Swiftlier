96ff16bf4b1e0a9e6f7263c55b270926793326f2
Andrew J Wagner <andrew@drewagllc.com>
2016-10-18 01:12:04 -0600

    [BF] Return empty array when decoding nil or null

--- SwiftPlusPlus/NativeTypesDecoder.swift
+++ SwiftPlusPlus/NativeTypesDecoder.swift
@@ -61,8 +61,14 @@ public final class NativeTypesDecoder: DecoderType {
     }
 
     public func decodeArray<Value: EncodableType>(key: NestedCoderKey<Value>.Type) -> [Value] {
-        guard let array = self.object(forKeyPath: key.path) as? [AnyObject] else {
-            fatalError("Unexpected type")
+        let object = self.object(forKeyPath: key.path)
+        guard let array = object as? [AnyObject] else {
+            if object is NSNull || object == nil {
+                return []
+            }
+            else {
+                fatalError("Unexpected type")
+            }
         }
         var output: [Value] = []
         for raw in array {
