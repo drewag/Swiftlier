84ff60a0aa7da981dfea783e9b1001bd6372882b
Andrew Wagner <andrew@onbeat.fit>
2016-11-04 21:01:56 -0600

    [BF] Fixed linux build

--- Sources/ShellCommand.swift
+++ Sources/ShellCommand.swift
@@ -16,18 +16,38 @@ public struct ShellCommand: CustomStringConvertible {
     }
 
     public func execute() -> String {
-        let process = Process()
+        #if os(iOS)
+            let process = Process()
 
-        process.launchPath = "/usr/bin/env"
-        process.arguments = self.command
+            process.launchPath = "/usr/bin/env"
+            process.arguments = self.command
 
-        let outputPipe = Pipe()
-        process.standardOutput = outputPipe
+            let outputPipe = Pipe()
+            process.standardOutput = outputPipe
 
-        process.launch()
+            process.launch()
 
-        let outputString = String(data: outputPipe.fileHandleForReading.availableData, encoding: .utf8) ?? ""
-        return outputString
+            let outputString = String(data: outputPipe.fileHandleForReading.availableData, encoding: .utf8) ?? ""
+            return outputString
+        #else
+            let BUFSIZE = 1024
+            let fullCommand = "/bin/sh -c \'\(command)\'"
+            let pp = popen(fullCommand, "r")
+            var buf = [CChar](repeating: CChar(0), count:BUFSIZE)
+
+            var output = ""
+            while fgets(&buf, Int32(BUFSIZE), pp) != nil {
+                output += String(cString: buf)
+            }
+            guard !output.isEmpty else {
+                return output
+            }
+            if output.characters.last == "\n" {
+                return output.substring(to: output.characters.count - 2)
+            } else {
+                return output
+            }
+        #endif
     }
 
     public var description: String {
