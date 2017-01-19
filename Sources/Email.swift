9c9a6a77f0c0f91834a4947def835f38c56205ba
Andrew Wagner <andrew@drewagllc.com>
2017-01-18 21:54:50 -0700

    [BF] Fixed email generation to not be so senstivie about acceptable characters

--- Sources/Email.swift
+++ Sources/Email.swift
@@ -17,7 +17,7 @@ public struct Email {
 
     public init(to: String, subject: String, from: String, HTMLBody: String) {
         self.recipient = to
-        self.HTMLBody = HTMLBody.replacingOccurrences(of: "'", with: "\\\\'")
+        self.HTMLBody = HTMLBody
         self.subject = subject
         self.from = from
     }
@@ -26,13 +26,22 @@ public struct Email {
     public func send() -> Bool {
         #if os(Linux)
             print("Sending email to '\(self.recipient)' with subject '\(self.subject)'")
-            let task = Task()
-            task.launchPath = "/bin/sh"
-            task.arguments = ["-c", "echo '\(self.HTMLBody)' | mail '\(self.recipient)' -s '\(self.subject)' -a 'From:\(self.from)' -a 'Content-Type: text/html'"]
 
-            task.launch()
-            task.waitUntilExit()
-            return task.terminationStatus == 0
+            do {
+                FileService.default.createDirectory(at: URL(fileURLWithPath: "tmp"))
+                let tempPath = "tmp/email.html"
+                try self.HTMLBody.write(toFile: tempPath, atomically: true, encoding: .utf8)
+                let task = Task()
+                task.launchPath = "/bin/sh"
+                task.arguments = ["-c", "cat \(tempPath) | mail '\(self.recipient)' -s '\(self.subject)' -a 'From:\(self.from)' -a 'Content-Type: text/html'"]
+
+                task.launch()
+                task.waitUntilExit()
+                return task.terminationStatus == 0
+            }
+            catch {
+                return false
+            }
         #else
             print("Sent email to '\(self.recipient)' with subject '\(self.subject)' and body '\(self.HTMLBody)'")
             return true
