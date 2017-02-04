dc4f8d23cc5961215cbb29ea998afa5e3664c92d
Andrew J Wagner <andrew@drewagllc.com>
2017-02-03 21:46:10 -0700

    [NF] Added count property to enumerations with Int raw value

---
+++ Sources/Enum+Convenience.swift
@@ -0,0 +1,17 @@
+//
+//  Enum+Convenience.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 1/27/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+extension RawRepresentable where RawValue == Int {
+    public static var count: Int {
+        var counter = 0
+        while let _ = Self(rawValue: counter) {
+            counter += 1
+        }
+        return counter
+    }
+}
