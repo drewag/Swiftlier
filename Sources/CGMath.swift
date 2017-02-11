ab2a6405f051dfad1741ccd92f6818d086a85553
Andrew J Wagner <andrew@drewagllc.com>
2017-02-11 02:34:01 -0700

    [NF] Allow multiplying CGPoints

--- Sources/CGMath.swift
+++ Sources/CGMath.swift
@@ -61,6 +61,15 @@ public func -=( lhs: inout CGPoint, rhs: CGPoint) {
     lhs.y -= rhs.y
 }
 
+public func *(lhs: CGPoint, rhs: CGPoint) -> CGPoint {
+    return CGPoint(x: lhs.x * rhs.x, y: lhs.y * rhs.y)
+}
+
+public func *=( lhs: inout CGPoint, rhs: CGPoint) {
+    lhs.x *= rhs.x
+    lhs.y *= rhs.y
+}
+
 public func *(lhs: CGPoint, rhs: CGFloat) -> CGPoint {
     return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
 }
