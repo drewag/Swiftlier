7e65aebf042463af8579b61ce43eafaabcfc0ec2
Andrew J Wagner <andrew@drewagllc.com>
2017-02-22 21:39:33 -0700

    [NF] Added app idioms

---
+++ Sources/UIDevice+AppIdioms.swift
@@ -0,0 +1,24 @@
+//
+//  UIDevice+AppIdioms.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 2/22/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+#if os(iOS)
+public enum AppIdiom {
+    case phone, pad
+}
+
+extension UIDevice {
+    public var appIdiom: AppIdiom {
+        switch self.userInterfaceIdiom {
+        case .pad, .tv, .carPlay, .unspecified:
+            return .pad
+        case .phone:
+            return .phone
+        }
+    }
+}
+#endif
