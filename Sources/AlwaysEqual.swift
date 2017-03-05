a3d26212505bc8725094c0ae4c3ae4d9e35cb53a
Andrew J Wagner <andrew@drewagllc.com>
2017-03-05 00:46:44 -0700

    [NF] Added priorizied operation queue

---
+++ Sources/AlwaysEqual.swift
@@ -0,0 +1,14 @@
+//
+//  AlwaysEqual.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 3/4/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+public protocol AlwaysEqual: Equatable {}
+extension AlwaysEqual {
+    public static func ==(lhs: Self, rhs: Self) -> Bool {
+        return true
+    }
+}
---
+++ Sources/PrioritizedOperationQueue.swift
@@ -0,0 +1,219 @@
+//
+//  PrioritizedOperationQueue.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 3/4/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+public protocol PrioritizedOperationIdentifier: Equatable {
+    var name: String {get}
+}
+
+/**
+ Queue operations and execute them in an order based on their priority
+ 
+ Important Notes:
+ - If an operation fails, all remaining operations are canceled and notified on their `onError` callback
+ - When an operation is queued, operations are not automatically started. You must explicitly call `execute` to start operations
+ - All operation callbacks are executed on the main thread
+ - Operations are provided a child operation queue to queue dependent operations. If these operations are already queued (at a lower priority)
+ on the parent queue, they will be pulled from the parent queue and executed as a part of the current operation.
+*/
+public class PrioritizedOperationQueue {
+    public enum Priority: Int {
+        case automatic
+        case userInterface
+        case userRequested
+    }
+
+    fileprivate var operations = [AnyOperation]()
+    fileprivate var parentQueue: PrioritizedOperationQueue? = nil
+
+    public init() {}
+
+    fileprivate init(parent: PrioritizedOperationQueue) {
+        self.parentQueue = parent
+    }
+
+    fileprivate var onFinishedAllRemainingOperations: [(UserReportableResult<Void>) -> ()] = []
+    public let isExecuting = Observable(false)
+
+    public func queueOperation<Result, Identifier: PrioritizedOperationIdentifier>(
+        _ identifier: Identifier,
+        prioritizedAs priority: Priority,
+        execute: @escaping (_ childOperationQueue: PrioritizedOperationQueue, _ completion: @escaping (UserReportableResult<Result>) -> ()) -> (),
+        onError: ((UserReportableError) -> ())?,
+        onSuccess: ((Result) -> ())?
+        )
+    {
+        guard let existingOperation: Operation<Result, Identifier> = self.getOperationMatching(identifier: identifier) else {
+            let operation = Operation(identifier: identifier, execute: execute, onError: onError, onSuccess: onSuccess, priority: priority)
+            self.insert(new: operation)
+            return
+        }
+
+        if !existingOperation.isExecuting {
+            // Update the execution block to the latest version as long as it isn't already executing
+            existingOperation.execute = execute
+        }
+        existingOperation.addListeners(onError: onError, onSuccess: onSuccess)
+        self.insert(new: existingOperation)
+    }
+
+    public func execute(onFinishedAllRemainingOperations: ((UserReportableResult<Void>) -> ())? = nil) {
+        if let onFinishedAllRemainingOperations = onFinishedAllRemainingOperations {
+            self.onFinishedAllRemainingOperations.append(onFinishedAllRemainingOperations)
+        }
+
+        guard !self.isExecuting.value else {
+            return
+        }
+
+        self.executeRemainingOperations()
+    }
+}
+
+private protocol AnyOperation {
+    var priority: PrioritizedOperationQueue.Priority {set get}
+    var onError: [(UserReportableError) -> ()] {get set}
+    func execute(on queue: PrioritizedOperationQueue, onComplete: @escaping (UserReportableError?) -> ())
+}
+
+private class Operation<Result, Identifier: PrioritizedOperationIdentifier>: AnyOperation {
+    let identifier: Identifier
+
+    var isExecuting = false
+
+    var execute: (_ childOperationQueue: PrioritizedOperationQueue, _ completion: @escaping (UserReportableResult<Result>) -> ()) -> ()
+    var onError: [(UserReportableError) -> ()]
+    var onSuccess: [(Result) -> ()]
+    var priority: PrioritizedOperationQueue.Priority
+
+    init(
+        identifier: Identifier,
+        execute: @escaping (_ childOperationQueue: PrioritizedOperationQueue, _ completion: @escaping (UserReportableResult<Result>) -> ()) -> (),
+        onError: ((UserReportableError) -> ())?,
+        onSuccess: ((Result) -> ())?,
+        priority: PrioritizedOperationQueue.Priority
+        )
+    {
+        self.identifier = identifier
+        self.execute = execute
+        if let onError = onError {
+            self.onError = [onError]
+        }
+        else {
+            self.onError = []
+        }
+        if let onSuccess = onSuccess {
+            self.onSuccess = [onSuccess]
+        }
+        else {
+            self.onSuccess = []
+        }
+        self.priority = priority
+    }
+}
+
+private extension PrioritizedOperationQueue {
+    func getOperationMatching<Result, Identifier: PrioritizedOperationIdentifier>(identifier: Identifier) -> Operation<Result, Identifier>? {
+        for (index, existingOperation) in self.operations.enumerated() {
+            guard let existingOperation = existingOperation as? Operation<Result, Identifier>
+                , identifier == existingOperation.identifier
+                else
+            {
+                continue
+            }
+            self.operations.remove(at: index)
+            return existingOperation
+        }
+
+        return self.parentQueue?.getOperationMatching(identifier: identifier)
+    }
+
+    func insert(new: AnyOperation) {
+        for (index, operation) in self.operations.enumerated() {
+            if new.priority.rawValue <= operation.priority.rawValue {
+                continue
+            }
+
+            self.operations.insert(new, at: index)
+            return
+        }
+
+        self.operations.append(new)
+    }
+
+    func executeRemainingOperations() {
+        guard self.operations.count > 0 else {
+            self.reportFinishedRemainign(withResult: .success(()))
+            return
+        }
+        let next = self.operations.removeFirst()
+
+        next.execute(on: self) { error in
+            if let error = error {
+                self.cancelRemainingOperations(dueToError: error)
+                return
+            }
+
+            self.executeRemainingOperations()
+        }
+    }
+
+    func cancelRemainingOperations(dueToError error: UserReportableError) {
+        for operation in self.operations {
+            operation.report(error: error)
+        }
+        self.operations.removeAll()
+        self.reportFinishedRemainign(withResult: .error(error))
+    }
+
+    func reportFinishedRemainign(withResult result: UserReportableResult<Void>) {
+        for block in self.onFinishedAllRemainingOperations {
+            block(result)
+        }
+        self.onFinishedAllRemainingOperations.removeAll()
+    }
+}
+
+private extension AnyOperation {
+    func report(error: UserReportableError) {
+        for block in self.onError {
+            block(error)
+        }
+    }
+}
+
+private extension Operation {
+    func addListeners(onError: ((UserReportableError) -> ())?, onSuccess: ((Result) -> ())?) {
+        if let onError = onError {
+            self.onError.append(onError)
+        }
+        if let onSuccess = onSuccess {
+            self.onSuccess.append(onSuccess)
+        }
+    }
+
+    func report(successResultingIn result: Result) {
+        for block in self.onSuccess {
+            block(result)
+        }
+    }
+
+    func execute(on queue: PrioritizedOperationQueue, onComplete: @escaping (UserReportableError?) -> ()) {
+        let childQueue = PrioritizedOperationQueue(parent: queue)
+        self.execute(childQueue) { result in
+            switch result {
+            case .error(let error):
+                self.report(error: error)
+                onComplete(error)
+            case .success(let result):
+                self.report(successResultingIn: result)
+                onComplete(nil)
+            }
+        }
+        childQueue.execute()
+    }
+}
