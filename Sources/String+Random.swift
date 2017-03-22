828341c60f0d03aa656a5766e7ef808bba07ef04
Andrew J Wagner <andrew@drewagllc.com>
2017-03-21 18:55:00 -0600

    [NF] Added ability to generate a random string

---
+++ Sources/String+Random.swift
@@ -0,0 +1,29 @@
+//
+//  String+Random.swift
+//  ssm
+//
+//  Created by Andrew J Wagner on 3/18/17.
+//
+//
+
+import Foundation
+
+extension String {
+    public init(randomOfLength length: Int) {
+        let allowedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        let allowedCharsCount = UInt32(allowedChars.characters.count)
+
+        var output = ""
+        for _ in 0 ..< length {
+            #if os(Linux)
+                let randomNumber = Int(random()) % Int(allowedCharsCount)
+            #else
+                let randomNumber = Int(arc4random_uniform(allowedCharsCount))
+            #endif
+            let index = allowedChars.index(allowedChars.startIndex, offsetBy: randomNumber)
+            let newCharacter = allowedChars[index]
+            output.append(newCharacter)
+        }
+        self = output
+    }
+}
