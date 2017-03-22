167cb57b698ae54df39614440cdc1555e111907c
Andrew J Wagner <andrew@drewagllc.com>
2017-03-21 18:56:28 -0600

    [NF] Allow piping one shell command into another

--- Sources/ShellCommand.swift
+++ Sources/ShellCommand.swift
@@ -9,46 +9,80 @@
 #if os(Linux) || os(macOS)
 import Foundation
 
-public struct ShellCommand: CustomStringConvertible {
+public final class ShellCommand: CustomStringConvertible {
     private let command: [String]
 
-    public init(_ command: [String]) {
+    #if os(Linux)
+        private let process = Task()
+    #else
+        private let process = Process()
+    #endif
+
+    private let parentCommand: ShellCommand?
+
+    private init(_ command: [String], parentCommand: ShellCommand?, captureOutput: Bool) {
         self.command = command
+        self.parentCommand = parentCommand
+
+        self.process.arguments = self.command
+        self.process.launchPath = "/usr/bin/env"
+        self.process.standardInput = Pipe()
+        if let parentCommand = parentCommand {
+            parentCommand.process.standardOutput = self.process.standardInput
+        }
+        else if captureOutput {
+            self.process.standardOutput = Pipe()
+        }
     }
 
-    public func execute() -> String {
-        #if os(Linux)
-            let BUFSIZE = 1024
-            let fullCommand = "/bin/sh -c \'\(command)\'"
-            let pp = popen(fullCommand, "r")
-            var buf = [CChar](repeating: CChar(0), count:BUFSIZE)
+    public convenience init(_ command: [String], captureOutput: Bool = true) {
+        self.init(command, parentCommand: nil, captureOutput: captureOutput)
+    }
 
-            var output = ""
-            while fgets(&buf, Int32(BUFSIZE), pp) != nil {
-                output += String(cString: buf)
+    public convenience init(_ command: String, captureOutput: Bool = true) {
+        self.init(command.components(separatedBy: " "), parentCommand: nil, captureOutput: captureOutput)
+    }
+
+    private convenience init(_ command: String, parentCommand: ShellCommand?, captureOutput: Bool = true) {
+        self.init(command.components(separatedBy: " "), parentCommand: parentCommand, captureOutput: captureOutput)
+    }
+
+    @discardableResult
+    public func execute() throws -> String {
+        var commands = [ShellCommand]()
+        var command: ShellCommand? = self
+        repeat {
+            commands.append(command!)
+            command = command!.parentCommand
+        } while command != nil
+
+        for command in commands {
+            command.process.launch()
+        }
+
+        self.process.waitUntilExit()
+
+        guard self.process.terminationStatus == 0 else {
+            if let errorPipe = self.process.standardError as? Pipe {
+                let outputString = String(data: errorPipe.fileHandleForReading.availableData, encoding: .utf8) ?? ""
+                throw LocalUserReportableError(source: "ShellCommand", operation: "executing command", message: outputString, reason: .user)
             }
-            guard !output.isEmpty else {
-                return output
+            else {
+                throw LocalUserReportableError(source: "ShellCommand", operation: "executing command", message: "", reason: .user)
             }
-            if output.characters.last == "\n" {
-                return output.substring(to: output.characters.count - 2)
-            } else {
-                return output
-            }
-        #else
-            let process = Process()
+        }
 
-            process.launchPath = "/usr/bin/env"
-            process.arguments = self.command
-
-            let outputPipe = Pipe()
-            process.standardOutput = outputPipe
-
-            process.launch()
-
+        if let outputPipe = process.standardOutput as? Pipe {
             let outputString = String(data: outputPipe.fileHandleForReading.availableData, encoding: .utf8) ?? ""
             return outputString
-        #endif
+        }
+        else {
+            return ""
+        }
+    }
+
+    public func pipe(to: String) -> ShellCommand {
+        return ShellCommand(to, parentCommand: self)
     }
 
     public var description: String {
