d07c8e1ac063887ba5a43d8b91a239cb641dd60e
Andrew J Wagner <andrew@drewagllc.com>
2017-04-04 20:11:49 -0600

    [NF] Added energy and heart rate formatters

---
+++ Sources/EnergyFormatting.swift
@@ -0,0 +1,29 @@
+//
+//  EnergyFormatting.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 4/3/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+import Foundation
+
+private let energyFormatter = EnergyFormatter()
+
+extension Double {
+    public var energyFromCalries: String {
+        return energyFormatter.string(fromValue: self, unit: .calorie)
+    }
+}
+
+extension Int {
+    public var energyFromCalories: String {
+        return Double(self).energyFromCalries
+    }
+}
+
+extension Float {
+    public var energyFromCalories: String {
+        return Double(self).energyFromCalries
+    }
+}
---
+++ Sources/HeartRateFormatter.swift
@@ -0,0 +1,18 @@
+//
+//  HeartRateFormatter.swift
+//  SwiftPlusPlus
+//
+//  Created by Andrew J Wagner on 4/2/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+import Foundation
+
+public class HeartRateFormatter: Formatter {
+    override public func string(for obj: Any?) -> String? {
+        if let number = obj {
+            return "\(number) bpm"
+        }
+        return nil
+    }
+}
