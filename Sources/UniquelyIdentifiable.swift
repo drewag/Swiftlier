3b98f3f7d344d8706c9f922392973f5217ae073e
Andrew J Wagner <drewag@gmail.com>
2017-10-05 11:30:56 -0600

    [NF] Add UniquelyIdentifiable protocol

---
+++ Sources/UniquelyIdentifiable.swift
@@ -0,0 +1,11 @@
+//
+//  File.swift
+//  Swiftlier
+//
+//  Created by Andrew J Wagner on 10/5/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+public protocol UniquelyIdentifiable {
+    var id: UUID {get}
+}
