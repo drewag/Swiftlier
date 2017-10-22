177139a36c1765d8e72f5cb5bc1afe9440e090ea
Andrew J Wagner <drewag@gmail.com>
2017-10-21 23:40:20 -0600

    [NF] Make rounded border button public

--- Sources/RoundedBorderButton.swift
+++ Sources/RoundedBorderButton.swift
@@ -9,26 +9,31 @@
 #if os(iOS)
 import UIKit
 
-@IBDesignable class RoundedBorderButton: UIButton {
-    @IBInspectable var borderColor: UIColor = UIColor.clear {
+@IBDesignable
+public class RoundedBorderButton: UIButton {
+
+    @IBInspectable
+    public  var borderColor: UIColor = UIColor.clear {
         didSet {
             self.update()
         }
     }
 
-    @IBInspectable var borderSize: CGFloat = 1 / UIScreen.main.scale {
+    @IBInspectable
+    public var borderSize: CGFloat = 1 / UIScreen.main.scale {
         didSet {
             self.update()
         }
     }
 
-    @IBInspectable var cornerRadius: CGFloat = 10 {
+    @IBInspectable
+    public var cornerRadius: CGFloat = 10 {
         didSet {
             self.update()
         }
     }
 
-    override func awakeFromNib() {
+    open override func awakeFromNib() {
         super.awakeFromNib()
 
         self.update()
