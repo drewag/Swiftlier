c50da9651e9beaa853f33dd0106479429b0b82e3
Andrew Wagner <coding-git@drewag.me>
2017-12-30 21:01:51 -0700

    [NF] Add testable extension to Date

---
+++ Sources/Date+Testable.swift
@@ -0,0 +1,35 @@
+//
+//  Date+Testable.swift
+//  Swiftlier
+//
+//  Created by Andrew Wagner on 12/30/17.
+//  Copyright © 2017 Drewag. All rights reserved.
+//
+
+import Foundation
+
+private var faked: (base: Date, started: Date)?
+
+extension Date {
+    public static func startFakingNow(from: Date) {
+        faked = (from, Date())
+    }
+
+    public static func stopFakingNow() {
+        faked = nil
+    }
+
+    public static func addFake(interval: TimeInterval) {
+        if let old = faked {
+            faked = (old.base.addingTimeInterval(interval), started: old.started)
+        }
+    }
+
+    public static var now: Date {
+        guard let faked = faked else {
+            return Date()
+        }
+        let interval = Date().timeIntervalSince(faked.started)
+        return faked.base.addingTimeInterval(interval)
+    }
+}
