883785ab2d4de36e97d7ffa3932612b80732d99e
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-06-09 08:22:18 -0600

    [NF] Add modelName to UIDevice

--- Sources/UIDevice+AppIdioms.swift
+++ Sources/UIDevice+AppIdioms.swift
@@ -22,5 +22,15 @@ extension UIDevice {
             return .phone
         }
     }
+
+    public var modelName: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        return machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+    }
 }
 #endif
