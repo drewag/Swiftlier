189c6b6317e5197f9e96bf759e1cbb13afb8fcca
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-09-11 23:25:42 -0600

    [NF] Allow rounding only certain corners on RoundedBorderView

--- Sources/RoundedBorderView.swift
+++ Sources/RoundedBorderView.swift
@@ -28,6 +28,11 @@ import UIKit
         }
     }
 
+    @IBInspectable public var roundTopLeft: Bool = true
+    @IBInspectable public var roundTopRight: Bool = true
+    @IBInspectable public var roundBottomLeft: Bool = true
+    @IBInspectable public var roundBottomRight: Bool = true
+
     override open func awakeFromNib() {
         super.awakeFromNib()
 
@@ -44,6 +49,23 @@ private extension RoundedBorderView {
         self.layer.shadowColor = UIColor.black.cgColor
         self.layer.shadowOpacity = 0.3
         self.layer.shadowOffset = CGSize()
+
+        if #available(iOS 11.0, *) {
+            var corners = CACornerMask()
+            if self.roundTopLeft {
+                corners.insert(.layerMinXMinYCorner)
+            }
+            if self.roundTopRight {
+                corners.insert(.layerMaxXMinYCorner)
+            }
+            if self.roundBottomLeft {
+                corners.insert(.layerMinXMaxYCorner)
+            }
+            if self.roundBottomRight {
+                corners.insert(.layerMaxXMaxYCorner)
+            }
+            self.layer.maskedCorners = corners
+        }
     }
 }
 #endif
