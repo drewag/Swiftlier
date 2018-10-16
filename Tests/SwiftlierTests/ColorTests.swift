c5ae45da0426e09534570173199a935bd86879a5
Andrew J Wagner <845683+drewag@users.noreply.github.com>
2018-10-16 13:49:32 -0600

    [BF] Fix tests

--- Tests/SwiftlierTests/ColorTests.swift
+++ Tests/SwiftlierTests/ColorTests.swift
@@ -22,14 +22,15 @@ class ColorTests: XCTestCase {
         XCTAssertEqual(try! UIColor(hexString: "#FF0000"), UIColor.red)
         XCTAssertEqual(try! UIColor(hexString: "#CC"), UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1))
         XCTAssertEqual(try! UIColor(hexString: "#CCC"), UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1))
+        XCTAssertEqual(try! UIColor(hexString: "FFFFFF"), UIColor(red: 1, green: 1, blue: 1, alpha: 1))
+        XCTAssertEqual(try! UIColor(hexString: "FF0000"), UIColor.red)
+        XCTAssertEqual(try! UIColor(hexString: "CC"), UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1))
+        XCTAssertEqual(try! UIColor(hexString: "CCC"), UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 1))
 
         XCTAssertThrowsError(try UIColor(hexString: "#F"))
         XCTAssertThrowsError(try UIColor(hexString: "#FFFF"))
         XCTAssertThrowsError(try UIColor(hexString: "#FFFFF"))
         XCTAssertThrowsError(try UIColor(hexString: "#FFFFFFF"))
-        XCTAssertThrowsError(try UIColor(hexString: "FF"))
-        XCTAssertThrowsError(try UIColor(hexString: "FFF"))
-        XCTAssertThrowsError(try UIColor(hexString: "FFFFFF"))
     }
 }
 #endif
