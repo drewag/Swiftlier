6a272ec12ca2cb8fa2853a8fe5ce31c512e9cbe8
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-07-07 22:45:45 -0600

    [NF] Optional navigation approval block for WebViewController

--- Sources/WebViewController.swift
+++ Sources/WebViewController.swift
@@ -8,27 +8,31 @@
 
 #if os(iOS)
 import UIKit
+import WebKit
 
 public class WebViewController: UIViewController, ErrorGenerating {
     let activityOverlay = UIActivityIndicatorView(style: .whiteLarge)
-    let webView = UIWebView()
+    let webView = WKWebView()
 
     enum Content {
         case url(URL)
         case html(String)
     }
     let content: Content
+    let shouldNavigateToUrl: ((URL) -> Bool)?
 
-    public init(URL: URL) {
+    public init(URL: URL, shouldNavigateToUrl: ((URL) -> Bool)? = nil) {
         self.content = .url(URL)
+        self.shouldNavigateToUrl = shouldNavigateToUrl
 
         super.init(nibName: nil, bundle: nil)
 
         self.sharedInit()
     }
 
-    public init(HTML: String) {
+    public init(HTML: String, shouldNavigateToUrl: ((URL) -> Bool)? = nil) {
         self.content = .html(HTML)
+        self.shouldNavigateToUrl = shouldNavigateToUrl
 
         super.init(nibName: nil, bundle: nil)
 
@@ -49,7 +53,7 @@ public class WebViewController: UIViewController, ErrorGenerating {
 
 private extension WebViewController {
     func sharedInit() {
-        self.webView.delegate = self
+        self.webView.navigationDelegate = self
         self.activityOverlay.color = UIColor.darkGray
         self.activityOverlay.hidesWhenStopped = true
 
@@ -60,23 +64,35 @@ private extension WebViewController {
         switch self.content {
         case .url(let url):
             let request = URLRequest(url: url)
-            self.webView.loadRequest(request)
+            self.webView.load(request)
         case .html(let html):
             self.webView.loadHTMLString(html, baseURL: nil)
         }
     }
 }
 
-extension WebViewController: UIWebViewDelegate {
-    public func webViewDidStartLoad(_ webView: UIWebView) {
+extension WebViewController: WKNavigationDelegate {
+    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        guard let url = navigationAction.request.url
+            , let shouldNavigate = self.shouldNavigateToUrl
+            , !shouldNavigate(url)
+            else
+        {
+            decisionHandler(.allow)
+            return
+        }
+        decisionHandler(.cancel)
+    }
+
+    public func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
         self.activityOverlay.startAnimating()
     }
 
-    public func webViewDidFinishLoad(_ webView: UIWebView) {
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         self.activityOverlay.stopAnimating()
     }
 
-    public func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+    public func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
         self.activityOverlay.stopAnimating()
         self.showAlert(withError: error, "loading", other: [
             .action("OK", handler: {
